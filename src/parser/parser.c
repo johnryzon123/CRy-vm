7b090a65b65e4d012d0b7ffb8b9d4da43022ca1b
Nguyễn Phước Thành Lâm <phuocthanhlamnguyen@gmail.com>
2026-07-14 10:31:08 +0700

    Change the arrows to an old-school dot
    IDK why, maybe I need to stop giving my weird ideas into play

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdlib.h> //Allocating memory heh, like allocating a barn
 #include "parser/ast_node.h"
 #include "parser/ast.h"
 #include "parser/parser.h"
@@ -6,19 +6,24 @@
 #include "lexer/token.h"
 
 void initParser(Parser* parser, Token** tokens) {
-  parser->tokens = *tokens;
-  parser->current = 0;
-  parser->asts.buffer = 10;
-  parser->asts.used = 0;
-  parser->asts.nodes = malloc(sizeof(ASTNode) * parser->asts.buffer);
+  //I get rid of the arrow
+  (*parser).tokens = *tokens;
+  (*parser).current = 0;
+  (*parser).asts.buffer = 10;
+  (*parser).asts.used = 0;
+  (*parser).asts.nodes = malloc(sizeof(ASTNode) * (*parser).asts.buffer);
+
+  if ((*parser).asts.nodes == NULL){ //If memory failed, then exit , common sense right =)
+    exit(1); //TO exit
+  }
 }
 
 void parse(Parser* parser) {
-  while (parser->tokens[parser->current].type != TOK_EOF) {
-    if (parser->errornow.type == PARSE_ERR)
+  while ((*parser).current < total_tokens && (*parser).tokens[(*parser).current].type != TOK_EOF) {
+    if ((*parser).errornow.type == PARSE_ERR){
       break;
-
-    int root_node_index = parse_statement(parser);
+    }
+    int root_node_index = parse_statement(parser); // IDK what it do, let me check
         
     if (root_node_index == -1 || root_node_index == -2) {
       break;
@@ -26,3 +31,6 @@ void parse(Parser* parser) {
         
   }
 }
+/*Joke of the day: Why doesn't an 11-year-old C dev want to program in CPP
+  Because he/she doesn't want to go to class
+*/
